47b0d9370355feea8d73b697890f18fd57287053
Ulaş Dündar <ulasdundar@hotmail.com>
2022-02-11 14:07:36 +0300

    webapicontroller eklendı proje olrak kaldırıldı

---
+++ SalesForce/Controllers/ApiServiceController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SalesForce.Controllers
+{ 
+    public class ApiServiceController : ApiController
+    {
+        public ApiServiceController()
+        {
+                
+        }
+        
+        public ModelUser CheckUserName(string username, string password)
+        {
+            UserManagement cs = new UserManagement();
+            ModelUser retVal = cs.checkUser(username, password);
+            return retVal;
+
+            //return "CheckUserName " + username  + "  " + password;
+        }
+    }
+}
